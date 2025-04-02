2f8549998861b90b39eab705d2b5701fc0db646f
ejting <ejting@ncsu.edu>
2025-04-02 13:00:32 -0400

    Working on fixing dialogue

--- scripts/interaction/interactable.gd
+++ scripts/interaction/interactable.gd
@@ -1,7 +1,11 @@
 class_name Interactable extends Node3D
 
+@export_category("Interactable Properties")
+@export var inter_name : String
+
 @export_category("Dialogue Objects")
 @export var dialogue_objects : Array[DialogueObject]
+
 
 ## This function will be called when interacted with
 func on_interact(dm_ref : DialogueManager):
