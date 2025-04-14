c014125e7517c2e49dfbf0f60108d631397c26ab
ejting <ejting@ncsu.edu>
2025-04-13 23:33:03 -0400

    added entire dentist building section

--- scripts/interaction/DialougeObject.gd
+++ scripts/interaction/DialougeObject.gd
@@ -4,4 +4,8 @@ class_name DialogueObject extends Resource
 @export_group("Dialogue Options")
 @export_multiline var dialogue : Array[String]
 @export_enum("Main") var voice_line : int
-@export_range(0.0, 2.0, 0.1) var dialouge_speed : float = 1.0
+@export_range(0.0, 2.0, 0.01) var dialouge_speed : float = 1.0
+
+@export_group("Response Options")
+@export var response_options_available : bool = false
+@export_multiline var response_button_titles : Array[String]
--- scripts/interaction/interactable.gd
+++ scripts/interaction/interactable.gd
@@ -2,10 +2,17 @@ class_name Interactable extends Node3D
 
 @export_category("Interactable Properties")
 @export var inter_name : String
+@export var self_thought : bool
 
 @export_category("Dialogue Objects")
 @export var dialogue_objects : Array[DialogueObject]
 
+# Index of the dialogue, specifically for the objects
+var dialogue_index : int = 0
+# Index for each line in that dialogue object
+var line_index : int = 0
+# A quick access variable for whether or not this object has response options
+var has_response_options : bool = false
 
 ## This function will be called when interacted with
 func on_interact(dm_ref : DialogueManager):
@@ -16,3 +23,10 @@ func stop_interact():
 
 func finished_dialouge(dm_ref : DialogueManager):
 	pass
+
+func set_interact_label_text(interact_label_ref : Label):
+	interact_label_ref.text = "Press [F] to interact"
+
+## Able to be overridden to make options do something other than close the prompt
+func option_selected(dm_ref : DialogueManager, which : int):
+	dm_ref.finish_message()
